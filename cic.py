d0939de8ad2b1ee9e544a9dc757c3b302d9e5b8b
Cookies2Eat <106758308+Cookies2Eat@users.noreply.github.com>
2022-06-03 11:29:35 +0200

    Update cic.py
    - watering pseudocode
    - variablennamen
    - main method threadable

--- cic.py
+++ cic.py
@@ -7,22 +7,28 @@ import configparser
 POLL_RATE_SECONDS = 4
 FLOWER_BEDS = 2
 SOIL_HUMIDITY = 0.5
+WATERING_TIME = 5
 DISPLAY_LOG = True
 
 
 # return a random value between 0 and 1 for testing purposes
 def measureHumidityLevels():
-    returns = []
+    humidities = []
     for i in range(FLOWER_BEDS):
         # TODO sensor-using
         # Random values
-        returns.append(round(random.uniform(0, 1), 2))
-    return returns
+        humidities.append(round(random.uniform(0, 1), 2))
+    return humidities
 
 
 # returns True, when watering was successful
 def watering(bedIndex):
-    #do something
+    # start pump
+    # open valve with bedIndex
+    time.sleep(WATERING_TIME)
+    # stop pump
+    # close all valves
+    # check if water is flowed    
     return True
 
 
@@ -31,21 +37,24 @@ def log(message):
         print(time.asctime(), "-->", message)
 
 
-# main routine
-while True:
-    log("Routine started.")
-    humidities = measureHumidityLevels()
-    log("Humidity levels: " + str(humidities))
-    for bedIndex, humidity in enumerate(humidities):
-        if humidity < SOIL_HUMIDITY:
-            # water the bed
-            log("Flower box #" + str(bedIndex) + " is to dry. Humidity: " + str(humidity))
-            if watering(bedIndex):
-                log("Flower bed #" + str(bedIndex) + " successfully watered.")
+def main():
+    while True:
+        log("Routine started.")
+        humidities = measureHumidityLevels()
+        log("Humidity levels: " + str(humidities))
+        for bedIndex, humidity in enumerate(humidities):
+            if humidity < SOIL_HUMIDITY:
+                # water the bed
+                log("Flower box #" + str(bedIndex) + " is to dry. Humidity: " + str(humidity))
+                if watering(bedIndex):
+                    log("Flower bed #" + str(bedIndex) + " successfully watered.")
+                else:
+                    log("Error during watering")
             else:
-                log("Error during watering")
-        else:
-            # ignore this flower bed
-            log("Flower box #" + str(bedIndex) + " is still wet enough. Humidity: " + str(humidity))
-    log("Routine finished.\n")
-    time.sleep(POLL_RATE_SECONDS)
+                # ignore this flower bed
+                log("Flower box #" + str(bedIndex) + " is still wet enough. Humidity: " + str(humidity))
+        log("Routine finished.\n")
+        time.sleep(POLL_RATE_SECONDS)
+
+if __name__ == "__main__":
+    main()
